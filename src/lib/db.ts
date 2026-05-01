f8ab7272424531f318a9027726c2ad64138b4709
Eli <okara.elisha@gmail.com>
2026-05-01 09:24:24 +0100

    fix casing issue in db config

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
 
 // Single Prisma instance across hot reloads in dev.
@@ -10,7 +10,7 @@ const libsql = createClient({
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
-const adapter = new PrismaLibSQL(libsql);
+const adapter = new PrismaLibSql(libsql);
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
